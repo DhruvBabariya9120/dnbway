9b0ae0dda82c0f2b73099393212b1e028d4fa7da
emekaiwuagwu <iwuagwuemmy@yahoo.com>
2024-01-09 11:39:21 +0000

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require("express");
 const logger = require("morgan");
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
