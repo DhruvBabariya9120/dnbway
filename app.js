bca8f7831c488c9e9ac2b4a5796d3ff29eb782fc
emekaiwuagwu <iwuagwuemmy@yahoo.com>
2024-01-09 11:15:39 +0000

    added cors

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ const loanApplication = require("./routes/loanApplicationController");
 const messages = require("./routes/messageController");
 const fruits = require("./routes/fruitController");
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+ });
 
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
