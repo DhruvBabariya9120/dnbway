016b298c4e2bf74d4cd71060e8c6457414762b3f
dhruv <dhruvbabariya912001@gmail.com>
2024-06-02 19:30:33 +0530

    handle error

--- middlewares/error-handling.js
+++ middlewares/error-handling.js
@@ -9,6 +9,8 @@ export default async (err, req, res, next) => {
     if (err.name === 'UnauthorizedError') {
         return res.status(401).json({ message: 'Unauthorized' });
     }
+    console.log(err.name)
+    console.log(err)
     // Handle other types of errors
     res.status(500).json({ message: 'Internal Server Error' });
 }
